31ad685c073e178e7d577a65569f6b7200bb3850
Pepe Valdivia <jvaldivia@softweb.pe>
2018-11-12 13:49:50 -0500

    Key, update reset

--- src/main/java/handlers/KeyHandler.java
+++ src/main/java/handlers/KeyHandler.java
@@ -68,7 +68,14 @@ public class KeyHandler{
     String rpta = "";
     Database db = new Database();
     try {
-
+      int userId = Integer.parseInt(request.queryParams("user_id"));
+      UserKey s = UserKey.findFirst("user_id = ?", userId);
+      if (s == null){
+        rpta = "not_found";
+      }else{
+        s.set("reset", org.apache.commons.lang3.RandomStringUtils.randomAlphabetic(40));
+        s.saveIt();
+      }
     }catch (Exception e) {
       String[] error = {"It was not possible to reset update the reset user key", e.toString()};
       JSONObject rptaTry = new JSONObject();
